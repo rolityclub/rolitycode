46aac0a06f1cf394dfa4bd358ea0478ecd857527
rolityclub <119486016+rolityclub@users.noreply.github.com>
2022-12-19 13:46:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,6 @@ function DownloadQrImage() {
       letterRendering: 1,
 	  dpi : 300,
       allowTaint : false,
-	  width: 1000,
   		onrendered: function (canvas) {
         //document.body.appendChild(canvas)
   			canvas.toBlob(function (blob) {
